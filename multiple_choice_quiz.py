6049f0cd015f2648c8b4b3c0af820b214f286d97
huynhminhman95 <minhman812@gmail.com>
2025-06-13 15:23:40 +0700

    learning code choice quiz

--- multiple_choice_quiz.py
+++ multiple_choice_quiz.py
@@ -3,12 +3,14 @@ question = [
     "Cau 1. Doi bong nao ve nhi World cup 1994?\nA. Brazil \nB. Italia \nC. Duc\n",
     "Cau 2. Doi bong nao vo dich Wolrd cup 1998?\nA. Argentina \nB. Phap \nC. Brazil\n",
     "Cau 3. Nuoc nao la chu nha Wolrd cup 2002?\nA. Qatar \nB. Duc \nC. HanQuoc + Nhatban\n",
+    "Cau 4. Doi bong nao vo dich ngoai hang anh 2025?\nA. Arsenal \nB. Manchester City \nC. Liverpool\n",
 ]
 
 quizzes = [
     Quiz(question[0], "B"),
     Quiz(question[1], "B"),
     Quiz(question[2], "C"),
+    Quiz(question[3], "C")
 ]
 
 def run_quizzes(quizzes):
