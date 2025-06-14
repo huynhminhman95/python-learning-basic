ae47dd62245e7bc37e835e689087f4de6cb8b45b
huynhminhman95 <minhman812@gmail.com>
2025-06-14 22:45:18 +0700

    learning Udemy Tutorial.

---
+++ Udemy Tutorial/maths.py
@@ -0,0 +1,13 @@
+# floor division
+# nhan duoc so nguyen lon nhat nho hon hoac bang
+# N = D * (N // D) + (N % D)
+# N = 20 D = 3
+# 20 = 3 * 6 + 2
+# 20 // 3 = 6
+# 20 % 3 = 2
+
+N = 20
+D = 3
+
+print("floor divition: ", N // D)
+print("remainder divition: ", N % D)
